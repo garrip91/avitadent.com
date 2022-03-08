a85e613e9bca53030b5a1f2d04c5ff63728ff3fd
garrip91 <45920127+garrip91@users.noreply.github.com>
2022-03-08 12:16:46 +0300

    Update admin.py

--- src/AvitaDentApp/admin.py
+++ src/AvitaDentApp/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 
 from .models import Services, Stocks
 
+from django.utils.safestring import mark_safe
+
 
 
 # Register your models here.
@@ -32,7 +34,14 @@ class StocksAdmin(admin.ModelAdmin):
         # return F'{obj.comment[:100]}...'
     # get_short_comment.short_description = 'comment'
     
-    list_display = ('Stocks_title', 'Stocks_gallery')
+    def get_image(self, obj):
+        return mark_safe(F'<img src={obj.Stocks_gallery.url} width="100" height="100">')
+    get_image.short_description = 'Изображение'    
+    
+    #list_display = ('Stocks_title', 'Stocks_gallery')
+    list_display = ('Stocks_title', 'get_image')
+    #readonly_fields = ('Stocks_gallery',)
+    readonly_fields = ('get_image',)
     
     
 admin.site.register(Services, ServicesAdmin)
