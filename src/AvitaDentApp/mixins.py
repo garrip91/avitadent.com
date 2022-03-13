89bae77af70954d16011cf8c45661e6ad10f81db
garrip91 <45920127+garrip91@users.noreply.github.com>
2022-03-13 22:42:33 +0300

    Update mixins.py

--- src/AvitaDentApp/mixins.py
+++ src/AvitaDentApp/mixins.py
@@ -11,33 +11,21 @@ class MyFormMixin(View):
 
     def dispatch(self, request, *args, **kwargs):
         #################### РЕАЛИЗУЕМ ПОЛУЧЕНИЕ ЗАЯВКИ ОТ ПОЛЬЗОВАТЕЛЯ: ####################
-        # if request.method == 'POST' and 'feedback' in request.POST:
-            # feedback_form = FeedbackForm(request.POST)
-            # if feedback_form.is_valid():
-                # feedback_form.save()
-                # Feedback_name = feedback_form.cleaned_data.get('Feedback_name')
-                # print(Feedback_name)
-                # Feedback_phone = feedback_form.cleaned_data.get('Feedback_phone')
-                # print(Feedback_phone)
-                # print("ВАША ЗАЯВКА УСПЕШНО ОТПРАВЛЕНА!")
-                # messages.success(request, "ВАША ЗАЯВКА УСПЕШНО ОТПРАВЛЕНА!")
-                # return HttpResponseRedirect(self.request.path)
-            # else:
-                # print("ЧТО-ТО ПОШЛО НЕ ТАК!")
-                # messages.error(request, 'НЕПРАВИЛЬНО ВВЕДЁН НОМЕР ТЕЛЕФОНА!')
-                # return HttpResponseRedirect(self.request.META.get('HTTP_REFERER'))
-        # else:
-            # self.feedback_form = FeedbackForm()
         if request.method == 'POST' and 'feedback' in request.POST:
             feedback_form = FeedbackForm(request.POST)
-            feedback_form.save()
-            Feedback_name = feedback_form.cleaned_data.get('Feedback_name')
-            print(Feedback_name)
-            Feedback_phone = feedback_form.cleaned_data.get('Feedback_phone')
-            print(Feedback_phone)
-            print("ВАША ЗАЯВКА УСПЕШНО ОТПРАВЛЕНА!")
-            messages.success(request, "ВАША ЗАЯВКА УСПЕШНО ОТПРАВЛЕНА!")
-            return HttpResponseRedirect(self.request.path)
+            if feedback_form.is_valid():
+                feedback_form.save()
+                Feedback_name = feedback_form.cleaned_data.get('Feedback_name')
+                print(Feedback_name)
+                Feedback_phone = feedback_form.cleaned_data.get('Feedback_phone')
+                print(Feedback_phone)
+                print("ВАША ЗАЯВКА УСПЕШНО ОТПРАВЛЕНА!")
+                messages.success(request, "ВАША ЗАЯВКА УСПЕШНО ОТПРАВЛЕНА!")
+                return HttpResponseRedirect(self.request.path)
+            else:
+                print("ЧТО-ТО ПОШЛО НЕ ТАК!")
+                messages.error(request, 'НЕПРАВИЛЬНО ВВЕДЁН НОМЕР ТЕЛЕФОНА!')
+                return HttpResponseRedirect(self.request.META.get('HTTP_REFERER'))
         else:
             self.feedback_form = FeedbackForm()
         #####################################################################################
