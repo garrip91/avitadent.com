646171dfac119e81ed7e68706a063a7fe3b65f70
garrip91 <45920127+garrip91@users.noreply.github.com>
2022-07-23 15:23:28 +0300

    Summary

--- src/AvitaDentProject/settings.py
+++ src/AvitaDentProject/settings.py
@@ -14,6 +14,8 @@ from pathlib import Path
 
 from . import Yandex_config
 
+from . SECRET_KEY import KEY
+
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -24,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ie5j&vriqesi(1zkh4%&p_hycmcqqbk$*q4ew(h7(j(^e&^w@*'
+SECRET_KEY = KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
